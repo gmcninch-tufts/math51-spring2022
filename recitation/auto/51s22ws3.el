5a133d66e8cd0429c9ddb2ddfc6524b9a62d6afc
George McNinch <george.mcninch@tufts.edu>
2022-02-05 09:56:45 -0500

    ws3

---
+++ recitation/auto/51s22ws3.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "51s22ws3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=.9in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "amsmath"
+    "amssymb"
+    "amscd"
+    "geometry"
+    "palatino"
+    "mathpazo"
+    "color")
+   (TeX-add-symbols
+    '("tred" 1)))
+ :latex)
+
